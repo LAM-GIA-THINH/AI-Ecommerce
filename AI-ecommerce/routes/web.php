1c7fc502d775a8ca5c9eacfd4a969fbdb99d55d6
LAM-GIA-THINH <giathinh961617@gmail.com>
2024-04-27 11:55:22 +0700

    cai dat va tao layouts

--- AI-ecommerce/routes/web.php
+++ AI-ecommerce/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Livewire\HomeComponent;
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', HomeComponent::class)->name('home.index');
